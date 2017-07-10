c5ebb157b57774b4a650d0775a47ed28035ccd6f
lxl <498357435@qq.com>
2017-07-10 22:36:43 +0800

    新增博客

--- myapp/views/blog_manage.py
+++ myapp/views/blog_manage.py
@@ -6,3 +6,7 @@ blog_manage = Blueprint("blog_manage", __name__)
 @blog_manage.route('/to_blog_manage')
 def to_blog_manage():
     return render_template("blog_manage/blog-manage-list.html")
+
+@blog_manage.route('/to_blog_add')
+def to_blog_add():
+    return render_template("blog_manage/article-add.html")

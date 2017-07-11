445273f25563c43fa145403196106b5aec69dace
lxl <498357435@qq.com>
2017-07-11 11:50:57 +0800

    暂存blog_manage

--- myapp/views/blog_manage.py
+++ myapp/views/blog_manage.py
@@ -1,7 +1,9 @@
 from flask import Flask, Blueprint, request, jsonify, render_template
+# from ueditor import UEditor
 
 blog_manage = Blueprint("blog_manage", __name__)
-
+# app = Flask(__name__)
+# ue = UEditor(app)
 
 @blog_manage.route('/to_blog_manage')
 def to_blog_manage():

6c31f68a0bab79f1c88193101895c06a4b8dcbe6
shanhq96 <shanhq96@gmail.com>
2017-07-11 17:35:37 +0800

    完成博客列表页的查询,添加数据库基础操作

--- myapp/views/blog_manage.py
+++ myapp/views/blog_manage.py
@@ -1,7 +1,12 @@
 from flask import Flask, Blueprint, request, jsonify, render_template
 from util.post_response import get_return_response
+# mongodb数据库连接
+from connect_db.connect_mongo import ObjectId, ConnectMongoDB
 
 blog_manage = Blueprint("blog_manage", __name__)
+
+connection = ConnectMongoDB()  # 连接数据库
+blog_collection = connection.get_collection('blog')
 
 
 @blog_manage.route('/to_blog_manage')
@@ -12,7 +17,7 @@ def to_blog_manage():
     return render_template("blog_manage/blog-manage-list.html")
 
 
-@blog_manage.route('/get_blog_manage_list', methods=['Post','Get' ])
+@blog_manage.route('/get_blog_manage_list', methods=['Post', 'Get'])
 def get_blog_manage_list():
     """
     获取博客列表页的datatable的数据
@@ -27,31 +32,35 @@ def get_blog_manage_list():
     currentPage = data_start / data_length + 1
     draw = int(form.get('draw'))
 
+    # 查询数据库
+    result_temp = connection.find_data(blog_collection)
+    result = result_temp.skip(data_start).limit(data_length)
+
     # 构造datables插件需要的配置数据
     response_datatables = {}
     response_datatables['draw'] = draw
-    response_datatables['recordsFiltered'] = test_totle_data_num
-    response_datatables['recordsTotal'] = test_totle_data_num
-
+    response_datatables['recordsFiltered'] = result_temp.count()
+    response_datatables['recordsTotal'] = result_temp.count()
 
     # 构造返回的data
+    ii = data_start
     response_data_array = []
-    for i in range(test_totle_data_num):
-        response_data = {}
-        response_data['id'] = i
-        response_data['title'] = 1
-        response_data['describe'] = 1
-        response_data['big_label'] = 1
-        response_data['small_label'] = 1
-        response_data['url'] = 1
-        response_data['create_time'] = 1
-        response_data['update_time'] = 1
-        response_data['hits'] = 1
-        response_data['is_top'] = 1
+    for response_data in result:
+        ii += 1
+        response_data['_id'] = str(response_data['_id'])
+        # response_data = {}
+        # response_data['id'] = i
+        response_data['title'] = ii
+        response_data['describe'] = ii
+        response_data['big_label'] = ii
+        response_data['small_label'] = ii
+        response_data['url'] = ii
+        response_data['create_time'] = ii
+        response_data['update_time'] = ii
+        response_data['hits'] = ii
+        response_data['is_top'] = ii<4
         response_data['is_status'] = 1
         response_data_array.append(response_data)
-
-
 
     response_datatables['data'] = response_data_array
     print(response_datatables)

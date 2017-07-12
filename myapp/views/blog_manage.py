e749cfc03579616874cdb7670c395f578cdec47a
lxl <498357435@qq.com>
2017-07-12 17:40:02 +0800

    新增博客大小标签

--- myapp/views/blog_manage.py
+++ myapp/views/blog_manage.py
@@ -1,7 +1,7 @@
 import time
 
 from flask import Blueprint, request, jsonify, render_template
-
+import time
 # mongodb数据库连接
 from connect_db.connect_mongo import ConnectMongoDB,ObjectId
 # from ueditor import UEditor
@@ -65,7 +65,24 @@ def to_blog_add():
     去博客新增页
     :return:
     """
+
     return render_template("blog_manage/article-add.html")
+
+@blog_manage.route('/get_taglist', methods=['Post', 'Get'])
+def get_taglist():
+    '''
+    获取博客标签列表
+    :return:
+    '''
+    print("标签列表")
+    result_taglist=connection.find_data(blog_label_collection,{"is_del":0})
+    response_data_array = []
+    for response_data in result_taglist:
+        response_data['_id'] = str(response_data['_id'])
+        response_data_array.append(response_data)
+    print(response_data_array)
+    response = get_return_response(jsonify(response_data_array))
+    return response
 
 
 @blog_manage.route('/save_blog', methods=['POST', 'GET'])
@@ -82,21 +99,21 @@ def save_blog():
     content = form.get('content')
     info = {}
     info['title'] = title  # 标题
-    info['big_label_id'] = onetype  # 一级分类
-    info['big_label_name'] = onetype  # 一级分类
-    info['small_label_id'] = twotype  # 二级分类
-    info['small_label_name'] = twotype  # 二级分类
+    info['big_label_id'] = onetype.split("@")[0]  # 一级分类
+    info['big_label_name'] = onetype.split("@")[1]  # 一级分类名称
+    info['small_label_id'] = twotype.split("@")[0]  # 二级分类
+    info['small_label_name'] = twotype.split("@")[1]  # 二级分类名称
     info['content'] = content  # 内容
     info['describe'] = maincontent  # 概要
     info['is_status'] = 0  # 0未发布
     info['create_time'] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))  # 创建时间
     info['update_time'] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))  # 更新时间
     info['title_img_url'] = ''  # 标题图片
+    info['url'] = ''  # 博客静态页
     info['hits'] = 0  # 点击次数
     info['is_top'] = False  # 是否置顶
     print(info['create_time'], info['title'])
     connection.insert_list(blog_collection, info)
-
     response = get_return_response(jsonify({"status": 1}))
     return response
 

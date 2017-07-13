70a4ff70ebf789025a8b208eb2c55995697c57ab
shanhq96 <shanhq96@gmail.com>
2017-07-13 19:12:02 +0800

    回退版本

--- myapp/util/webuploader.py
+++ myapp/util/webuploader.py
@@ -1,3 +1,4 @@
+import time
 from flask import Flask, Blueprint, request, jsonify, render_template
 from flask_login import login_required
 
@@ -9,8 +10,8 @@ webuploader = Blueprint("webuploader", __name__)
 def upload():
     print('请求')
     file = request.files['file']
-    
-
-
-    response = get_return_response(jsonify({"status": True}))
+    path = '/home/hadoop/PycharmProjects/170709/llws_blog_rest/myapp/static/title_imgs/'
+    filename = "%s%s"%(time.strftime('%Y%m%d%H%M%S', time.localtime(time.time())),file.filename[file.filename.rindex('.'):])
+    file.save(path + filename)
+    response = get_return_response(jsonify({"status": True,"data":{"title_img_url":path + filename}}))
     return response
